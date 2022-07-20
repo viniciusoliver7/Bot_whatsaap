a91446c71d2ed97c98d05b16c2125aa7f7ec6e94
viniciusoliver7 <103230924+viniciusoliver7@users.noreply.github.com>
2022-07-20 19:46:58 -0300

    Delete otWhatsaap directory

--- otWhatsaap/teste.py
+++
@@ -1,8 +0,0 @@
-with open("./funcoes_e_Dados/Lista1.csv",'r') as file:
-    for c in file:
-
-        if c =='112,67923,Dance With Me\n':
-            print('ok')
-        else:
-            pass
-    file.close()
