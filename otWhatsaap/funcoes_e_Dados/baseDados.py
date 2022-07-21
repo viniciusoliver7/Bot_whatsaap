09a853c19ac08043d459c7988002a1bef722940a
viniciusoliver7 <103230924+viniciusoliver7@users.noreply.github.com>
2022-07-21 20:22:07 -0300

    Delete otWhatsaap directory

--- otWhatsaap/funcoes_e_Dados/baseDados.py
+++
@@ -1,8 +0,0 @@
-conversas=("olá tudo bem ? me chamo Leticia  me informe seu nome por-favor",
-           "ok,agora me informe a sua mesa",
-           "quantas pessoas são",
-           "me informe os cogigos das musicas",
-           "muito obrigado tchau tchau"
-           )
-
-
